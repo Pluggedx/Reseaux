3cde567b26f35e63ade8850e1377a7df21d44945
Pluggedx <69322094+Pluggedx@users.noreply.github.com>
2023-01-31 21:04:41 -0500

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -19,7 +19,36 @@ DataInputStream in = new DataInputStream(socket.getInputStream());
 String helloMessageFromServer = in.readUTF();
 System.out.println(helloMessageFromServer);
 
+static private String demanderNomFichierUpload(BufferedReader reader) throws IOException{
+    String nomFichierUpload = null;
+    File fichierUpload = null;
+    boolean lisible = false;
+    while(!lisible){
+        System.out.print("Veuillez entrer le nom du fichier: ");
+        nomFichierUpload = reader.readLine();
+        fichierUpload = new File ("/." + nomFichierUpload);
+        lisible = fichierUpload.canRead();
 
+        if(!lisible){
+            System.out.format("\nAucun fichier comportant le nom entre. Veuillez entrer un autre nom de fichier \n");
+        }
+    }
+    return fichierUpload;
+    }
+static private void envoyerFichierUpload(
+    File fichierUpload,
+    ObjectOutputStream envoiFichierSocket) throws IOException, ClassNotFoundException {
+            byte [] bytesFichierUpload = new byte [(int)fichierUpload.length()];
+
+            BufferedInputStream inputStream = null;
+            inputStream = new BufferedInputStream(new FileInputStream(fichierUpload));
+            inputStream.read(bytesFichierUpload);
+            inputStream.close();
+            inputStream.read
+            envoiFichierSocket.writeObject(fichierUpload.getname());
+            envoiFichierSocket.writeObject(bytesFichierUpload);
+        }
+	
 String waitCommand = in.readUTF();
 System.out.println(waitCommand);
 	
